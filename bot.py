d8df01ecae1c29cedf285063232b1d9f8297bcd7
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-03-11 02:01:46 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -265,7 +265,7 @@ async def universal_downloader(client, callback_query: CallbackQuery):
         except Exception as e:
             await callback_query.edit_message_text(f"❌ Yükləmə xətası: {str(e)}")
 
-#wiki
+# wiki
 
 import wikipedia
 import random
